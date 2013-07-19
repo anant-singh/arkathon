59a4a386a0f7e41527dede8f5f732e380fa603ea
Akshay K Singh <akshaykr@eighlark.com>
2013-07-19 12:35:56 +0530

    Initial Commit

---
+++ src/main/java/com/eighlark/ark/arkathon/HomeController.java
@@ -0,0 +1,16 @@
+package com.eighlark.ark.arkathon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Welcome to Arkathon");
+		return "base";
+	}
+}
